2096ae4e2ada409494c5bee36810e5be49fb8f4c
arsenyyankovski <arseny.yankovsky@emarketeer.com>
2023-07-02 22:14:57 +0200

    feat: generate requirements.txt using pip freeze

--- ailess/modules/python_utils.py
+++ ailess/modules/python_utils.py
@@ -1,11 +1,23 @@
 import os
+import shutil
+from yaspin import yaspin
 
 from pipreqs.pipreqs import get_all_imports, get_pkg_names, get_import_local, get_imports_info
 
-
+def find_pip_command():
+    """
+    Finds the appropriate pip command based on availability.
+    Returns 'pip3' if available, otherwise 'pip'.
+    """
+    if shutil.which('pip3') is not None:
+        return 'pip3'
+    elif shutil.which('pip') is not None:
+        return 'pip'
+    else:
+        return None
 def ensure_requirements_exists():
     if not os.path.exists("requirements.txt"):
-        print("requirements.txt not found. Generating...")
+        print("requirements.txt not found. Generating using pip freeze. We recommend writing your own requirements.txt as this approach will include all dependencies, including those that are not used.")
         generate_requirements_file("./")
 
 
@@ -28,10 +40,12 @@ def get_libs(project_path):
 
 
 def generate_requirements_file(project_path):
-    libs = get_libs(project_path)
-    file_path = os.path.join(project_path, "requirements.txt")
-    requirements = ""
-    for lib in libs:
-        requirements += "{}=={}\n".format(lib["name"], lib["version"])
-    with open(file_path, "w") as file:
-        file.write(requirements)
+    from ailess.modules.cli_utils import run_command_in_working_directory
+    with yaspin(text="    generating requirements.txt") as spinner:
+        pip_command = find_pip_command()
+
+        if pip_command is None:
+            spinner.fail("pip not found. please install pip or create a valid requirements.txt file")
+            return
+
+        run_command_in_working_directory("{} freeze > requirements.txt".format(pip_command), spinner)
